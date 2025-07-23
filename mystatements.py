520e2dc738438f2f495060fedf72728bb0584b7e
holdengill <62360469+holdengill@users.noreply.github.com>
2025-07-23 20:08:58 +0000

    adding bs

---
+++ mystatements.py
@@ -0,0 +1,8 @@
+my_list = ['apple','cherry','lemon']
+my_dict = {'drink':'coffee','milk':'whole_milk'}
+
+for item in my_list:
+    print(f'fruit: {item}')
+
+for _,v in my_dict.items():
+    print(f'My favorite think to drink {v}')
